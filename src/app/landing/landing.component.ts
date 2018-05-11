a5414d14b95c2e4390bf96d6cd985368ec937471
aditioan <aditioan.uny@gmail.com>
2018-05-11 18:33:45 +0700

    add modal section

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,15 +1,36 @@
 // import * as $ from "jquery";
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-landing',
     templateUrl: './landing.component.html',
-    styleUrls: ['./landing.component.scss']
+    styleUrls: ['./landing.component.scss'],
 })
 
 export class LandingComponent implements OnInit {
 
-  	constructor() { }
+    closeResult: string;
 
-  	ngOnInit() {}
+    constructor(private modalService: NgbModal) {}
+
+    ngOnInit() {}
+  
+    open(content) {
+      this.modalService.open(content, {size: 'lg'}).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
+    }
+  
+    private getDismissReason(reason: any): string {
+      if (reason === ModalDismissReasons.ESC) {
+        return 'by pressing ESC';
+      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+        return 'by clicking on a backdrop';
+      } else {
+        return  `with: ${reason}`;
+      }
+    }
 }
